34b20a8cfb4f87d4ef3adbb1bda9738674d3a2c9
hwayoungBaek <wwwo3owww@naver.com>
2018-01-09 13:42:29 +0900

    Add files via upload

--- caller.js
+++ caller.js
@@ -20,15 +20,7 @@ btn_start.addEventListener('click', onStart);
 btn_finalOffer.addEventListener('click', onOffer);
 btn_receiveAnswer.addEventListener('click', onReceiveAnswer);
 
-var snapshotButton = document.querySelector('button#snapshot');
-var toggleMirrorButton = document.querySelector('button#toggle-mirror');
-var filterSelect = document.querySelector('select#filter');
-
-var canvas = window.canvas = document.querySelector('canvas');
-canvas.width = 480;
-canvas.height = 360;
-
-/*---------caller 에서도 desktop화면 보이도록------------*/
+/*-------------desktop 보이는 기능 caller에도 추가 -------------- */
 var btn_desktop = document.querySelector('#btn_desktop');
 var btn_toggle_video = document.querySelector('#btn_toggle_video');
 var btn_toggle_sound = document.querySelector('#btn_toggle_sound');
@@ -38,24 +30,6 @@ btn_desktop.addEventListener('click', onToggleDesktop);
 btn_toggle_video.addEventListener('click', onToggleVideo);
 btn_toggle_sound.addEventListener('click', onToggleSound);
 btn_toggle_mic.addEventListener('click', onToggleMic);
-
-snapshotButton.onclick = function() {
-    canvas.className = filterSelect.value;
-    canvas.getContext('2d').drawImage(vid1, 0, 0, canvas.width, canvas.height);
-};
-
-filterSelect.onchange = function() {
-    vid1.className = filterSelect.value;
-};
-
-var vidClassName = '';
-toggleMirrorButton.onclick = function() {
-    if (!vidClassName)
-        vidClassName = 'mirror';
-    else 
-        vidClassName = '';
-    vid1.className = vidClassName;
-};
 
 // ---------------------------------------------------------------------------------
 
@@ -92,7 +66,7 @@ function onStart() {
         iceServers: [
         ]
     };
-    // cfg.iceServers.push({urls: "stun:stun.l.google.com:19302"});
+    cfg.iceServers.push({urls: "stun:stun.l.google.com:19302"});
     local_peer = new RTCPeerConnection(cfg);
     local_peer.onicecandidate = function (evt) {
         cbIceCandidate(local_peer, evt);
@@ -187,56 +161,7 @@ function cbCheckIceCandidateCompleted(descObject) {
     output_offerDesc.value = descObject.sdp;
 }
 
-/*---------caller 에서도 desktop화면 보이도록------------*/
-function cbGotStream(stream) {
-    trace('Received local stream');
-    window.stream = stream;
-    vid1.srcObject = stream;
-    localstream = stream;
-
-    return navigator.mediaDevices.enumerateDevices();
-}
-function gotDevices(deviceInfos) {
-    // Handles being called several times to update labels. Preserve values.
-    var values = selectors.map(function(select) {
-      return select.value;
-    });
-    selectors.forEach(function(select) {
-      while (select.firstChild) {
-        select.removeChild(select.firstChild);
-      }
-    });
-    for (var i = 0; i !== deviceInfos.length; ++i) {
-      var deviceInfo = deviceInfos[i];
-      var option = document.createElement('option');
-      option.value = deviceInfo.deviceId;
-      if (deviceInfo.kind === 'audioinput') {
-        option.text = deviceInfo.label ||
-            'microphone ' + (audioInputSelect.length + 1);
-        audioInputSelect.appendChild(option);
-      } else if (deviceInfo.kind === 'audiooutput') {
-        option.text = deviceInfo.label || 'speaker ' +
-            (audioOutputSelect.length + 1);
-        audioOutputSelect.appendChild(option);
-      } else if (deviceInfo.kind === 'videoinput') {
-        option.text = deviceInfo.label || 'camera ' + (videoSelect.length + 1);
-        videoSelect.appendChild(option);
-      } else {
-        console.log('Some other kind of source/device: ', deviceInfo);
-      }
-    }
-    selectors.forEach(function(select, selectorIndex) {
-      if (Array.prototype.slice.call(select.childNodes).some(function(n) {
-        return n.value === values[selectorIndex];
-      })) {
-        select.value = values[selectorIndex];
-      }
-    });
-}
-function handleError(error) {
-    console.log('navigator.getUserMedia error: ', error);
-}
-navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
+/*-------------desktop 보이는 기능 caller에도 추가 -------------- */
 function startDesktop() {
     if (window.stream) {
         window.stream.getTracks().forEach(function(track) {
@@ -260,29 +185,6 @@ function startDesktop() {
         });
     });
 }
-// function start() {
-//     if (window.stream) {
-//       window.stream.getTracks().forEach(function(track) {
-//         track.stop();
-//       });
-//     }
-//     var audioSource = audioInputSelect.value;
-//     var videoSource = videoSelect.value;
-//     var constraints = {
-//       audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
-//       video: {deviceId: videoSource ? {exact: videoSource} : undefined}
-//     };
-
-//     navigator.mediaDevices.getUserMedia(constraints).
-//         then(cbGotStream).then(gotDevices).catch(handleError);
-// }
-
-// audioInputSelect.onchange = start;
-// audioOutputSelect.onchange = changeAudioDestination;
-// videoSelect.onchange = start;
-
-// start();
-/*---------------------------------------------------------- */
 
 var isDesktop = false;
 function onToggleDesktop(){
